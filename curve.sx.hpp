bed2a346a85b92242a59c437baaf9fdb3656dc3f
YaroShkvorets <shkvorets@gmail.com>
2021-01-25 13:26:54 -0500

    handle status and fees

--- curve.sx.hpp
+++ curve.sx.hpp
@@ -116,41 +116,8 @@ public:
     [[eosio::action]]
     void reset();
 
-    /**
-     * Notify contract when any token transfer notifiers relay contract
-     */
     [[eosio::on_notify("*::transfer")]]
     void on_transfer( const name from, const name to, const asset quantity, const std::string memo );
-
-private:
-    // token helpers
-    void create( const extended_symbol value );
-    void transfer( const name from, const name to, const extended_asset value, const string memo );
-    void retire( const extended_asset value, const string memo );
-    void issue( const extended_asset value, const string memo );
-
-    // helper to parse memo
-    pair<extended_asset, name> parse_memo(string memo);
-
-    // find pair_id based on symbol_code of incoming tokens and memo
-    symbol_code find_pair_id( const symbol_code symcode_in, const symbol_code symcode_memo );
-
-    // find all possible paths to trade symcode_in to memo symcode, include 2-hops
-    vector<vector<symbol_code>> find_trade_paths( symbol_code symcode_in, symbol_code symcode_memo );
-
-    //calculate out for trade via {path}, finalize it if {finalize}==true
-    extended_asset apply_trade( extended_asset ext_quantity, const vector<symbol_code> path, const uint8_t fee, bool finalize = false );
-
-    // normalizing input for tokens with different precision
-    int64_t mul_amount( const int64_t amount, const uint8_t precision0, const uint8_t precision1 );
-    int64_t div_amount( const int64_t amount, const uint8_t precision0, const uint8_t precision1 );
-
-    // maintenance
-    template <typename T>
-    void clear_table( T& table );
-
-public:
-
 
     /**
      * ## STATIC `get_amount_out`
@@ -179,8 +146,8 @@ public:
     static asset get_amount_out( const asset in, const symbol_code pair_id) {
 
         sx::curve::settings _config( sx::curve::code, sx::curve::code.value );
-        check( _config.exists(), "Curve.sx: contract is under maintenance");
-        auto fee = _config.get().fee;
+        check( _config.exists() && _config.get().status == "ok"_n, "Curve.sx: contract is under maintenance");
+        auto fee = _config.get().trade_fee + _config.get().protocol_fee;
 
         sx::curve::pairs _pairs( sx::curve::code, sx::curve::code.value );
         auto pool = _pairs.get( pair_id.raw(), "Curve.sx: invalid pair id" );
@@ -194,6 +161,33 @@ public:
 
         return out;
     }
+
+private:
+    // token helpers
+    void create( const extended_symbol value );
+    void transfer( const name from, const name to, const extended_asset value, const string memo );
+    void retire( const extended_asset value, const string memo );
+    void issue( const extended_asset value, const string memo );
+
+    // helper to parse memo
+    pair<extended_asset, name> parse_memo(string memo);
+
+    // find pair_id based on symbol_code of incoming tokens and memo
+    symbol_code find_pair_id( const symbol_code symcode_in, const symbol_code symcode_memo );
+
+    // find all possible paths to trade symcode_in to memo symcode, include 2-hops
+    vector<vector<symbol_code>> find_trade_paths( symbol_code symcode_in, symbol_code symcode_memo );
+
+    //calculate out for trade via {path}, finalize it if {finalize}==true
+    extended_asset apply_trade( extended_asset ext_quantity, const vector<symbol_code> path, const uint8_t fee, bool finalize = false );
+
+    // normalizing input for tokens with different precision
+    int64_t mul_amount( const int64_t amount, const uint8_t precision0, const uint8_t precision1 );
+    int64_t div_amount( const int64_t amount, const uint8_t precision0, const uint8_t precision1 );
+
+    // maintenance
+    template <typename T>
+    void clear_table( T& table );
 
 };
 }
